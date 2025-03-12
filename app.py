fa8874cb8ecc4aa174ff3d4b64678ef7efc653d4
NurStarUz <nurstarpul@gmail.com>
2025-03-12 22:13:16 +0500

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, jsonify
-import os
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -24,5 +23,4 @@ def ranking():
     return jsonify({"message": "TOP10 reyting"})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=10000)
